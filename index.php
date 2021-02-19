e75498a39dcfe32e82491e8b1a1ca417f8353b52
codepoet80 <curator@webosarchive.com>
2021-02-19 17:56:49 -0500

    change links

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ if (count($app_response["feeds"]) > 0)
     echo("</table>");
 }
 ?>
-    <p align='middle' style="margin-top: 12px"><small>Search Provided by <a href='https://podcastindex.org/'>Podcast Index.org</a> | <a href="https://github.com/codepoet80/retro-podcasts">Host this yourself</a> | Download the <a href='http://appcatalog.webosarchive.com/showMuseum.php?search=podcast+directory'>webOS App</a></small></p>
+    <p align='middle' style="margin-top: 28px"><small>Search Provided by <a href='https://podcastindex.org/'>Podcast Index.org</a> | <a href="https://github.com/codepoet80/retro-podcasts">Host this yourself</a> | <a href='http://appcatalog.webosarchive.com/showMuseum.php?search=podcast+directory'>Download the webOS App</a></small></p>
 
 </body>
 </html>
