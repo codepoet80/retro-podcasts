4b6525269aba90e5816b72ec9d601c34ec91355c
codepoet80 <curator@webosarchive.com>
2021-02-19 17:54:32 -0500

    a little more space

--- index.php
+++ index.php
@@ -59,6 +59,7 @@ if (count($app_response["feeds"]) > 0)
     echo("</table>");
 }
 ?>
+    <br>&nbsp;
     <p align='middle'><small>Search Provided by <a href='https://podcastindex.org/'>Podcast Index.org</a> | Download the <a href='http://appcatalog.webosarchive.com/showMuseum.php?search=podcast+directory'>webOS App</a></small></p>
 
 </body>
