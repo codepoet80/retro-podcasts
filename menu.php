247e69d64724408f395e0a714263e5beca4407dd
codepoet80 <jonathan.m.wise@gmail.com>
2022-01-22 13:33:30 -0500

    fix bug

--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 <?php
 //Figure out what protocol the client wanted
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
-$protocol = "https://";
+echo file_get_contents("https://www.webosarchive.com/menu.php?content=podcasts");
 else
-$protocol = "http://";
+echo file_get_contents("http://www.webosarchive.com/menu.php?content=podcasts");
 
-echo file_get_contents($protocol . "://www.webosarchive.com/menu.php?content=podcasts");
+
 ?>
