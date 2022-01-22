74bffff32018cf2bd9620d5cc4160d59f8e4084b
codepoet80 <jonathan.m.wise@gmail.com>
2022-01-22 13:34:16 -0500

    cleanup

--- menu.php
+++ menu.php
@@ -4,6 +4,4 @@ if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
 echo file_get_contents("https://www.webosarchive.com/menu.php?content=podcasts");
 else
 echo file_get_contents("http://www.webosarchive.com/menu.php?content=podcasts");
-
-
 ?>
