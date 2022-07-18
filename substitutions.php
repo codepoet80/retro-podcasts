ced314089fe869f8ee786b031c5bc1e2859188c9
codepoet <jon.wise@jonandnic.com>
2022-07-18 19:00:02 -0400

    Update substitutions.php

--- substitutions.php
+++ substitutions.php
@@ -1,10 +1,10 @@
 <?php
 $substitutions = array(
     "https://feeds.feedburner.com/TreocentralTreoCast" => (array) [
-        title => "PalmCast (Restored)",
-        url => "http://www.webosarchive.com/podcasts/feeds/PalmCast/feed.xml",
-        image => "http://www.webosarchive.com/podcasts/feeds/PalmCast/PalmCast.jpg",
-        substitution_reason => "This feed has been modified in order to restore broken download links"
+        'title' => "PalmCast (Restored)",
+        'url' => "http://www.webosarchive.com/podcasts/feeds/PalmCast/feed.xml",
+        'image' => "http://www.webosarchive.com/podcasts/feeds/PalmCast/PalmCast.jpg",
+        'substitution_reason' => "This feed has been modified in order to restore broken download links"
     ],
 );
 
